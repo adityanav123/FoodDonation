449a23acf6bf39a689612f9c4d6a529d111e5663
Aditya Navphule <39703082+adityanav123@users.noreply.github.com>
2020-06-22 14:50:45 +0530

    try to remove conflict

--- portal/urls.py
+++ portal/urls.py
@@ -3,11 +3,6 @@ from . import views
 
 
 urlpatterns = [
-
-    path('', views.index, name='portalIndex'),
     path('', views.index, name='portal_index'),
     path('register', views.register, name='register'),
-
-    path('', views.index, name='portal_index'),
-    path('register', views.register, name='register')
 ]
