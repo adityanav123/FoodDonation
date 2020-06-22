eef7985b930cfc9ba308b8332c9f7ef5b1ebe107
Kartik kumar merotha <kmerotha1996@gmail.com>
2020-06-22 12:48:53 +0530

    merge kartik

--- portal/urls.py
+++ portal/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from . import views
+
+
 urlpatterns = [
-<<<<<<< HEAD
     path('', views.index, name='portalIndex'),
-=======
-    path('', views.index, name = 'portal_index'),
-    path('register', views.register, name = 'register')   
->>>>>>> 22b634deb2160efe835c035679c01b1eeda7786e
+    path('', views.index, name='portal_index'),
+    path('register', views.register, name='register'),
 ]
