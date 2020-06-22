4bd4a36d050c07af4678e8a51c986699a770f393
Aditya Navphule <39703082+adityanav123@users.noreply.github.com>
2020-06-22 12:23:42 +0530

    Update urls.py

--- portal/urls.py
+++ portal/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name = 'portal_index'),
-    path('register', views.register, name = 'register')
+    path('register', views.register, name = 'register')   
 ]
